99535b72b8168035549c09294f0548a12a74dc30
Vadim Gan <vadim.gan2018@yandex.ru>
2023-10-22 00:53:54 +0300

    refactoring

--- Chess.Persistance/Intarfaces/IPlayerRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Chess.Domain.Models.User;
-
-namespace Chess.Persistance.Intarfaces
-{
-    internal interface IPlayerRepository : IRepository<Player> 
-    {
-        Player Get(string login);
-    }
-}
